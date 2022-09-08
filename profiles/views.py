754381edd5d9565935304cbd37508249d23bb52c
kkwong44 <wong.k@sky.com>
2022-09-08 22:51:28 +0000

    Create read and edit ProfileDetail view

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,8 @@
 '''
 Profiles Views
 '''
+from django.http import Http404
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
@@ -9,7 +11,7 @@ from .serializers import ProfileSerializer
 
 class ProfileList(APIView):
     '''
-    View for list of profiles
+    View for list of all profiles
     '''
     def get(self, request):
         '''
@@ -18,3 +20,39 @@ class ProfileList(APIView):
         profiles = Profile.objects.all()
         serializer = ProfileSerializer(profiles, many=True)
         return Response(serializer.data)
+
+
+class ProfileDetail(APIView):
+    '''
+    Detail view on a profile
+    '''
+    serializer_class = ProfileSerializer
+
+    def get_object(self, pk):
+        '''
+        Fetch profile by id
+        '''
+        try:
+            profile = Profile.objects.get(pk=pk)
+            return profile
+        except Profile.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        '''
+        Serialize profile
+        '''
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        '''
+        Edit profile
+        '''
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
