485438dea7178c309950b8c0e5c1206b5a4f0cf1
kkwong44 <wong.k@sky.com>
2022-09-16 23:45:33 +0000

    Update and add more test cases to albums

--- albums/tests.py
+++ albums/tests.py
@@ -4,6 +4,9 @@ Test cases for albums
 from rest_framework import status
 from rest_framework.test import APITestCase
 from django.contrib.auth.models import User
+from photos.models import Photo
+from comments.models import Comment
+from likes.models import Like
 from .models import Album
 
 
@@ -17,28 +20,44 @@ class AlbumListViewTests(APITestCase):
         '''
         User.objects.create_user(username='admin', password='pass')
 
-    def test_can_list_posts(self):
+    def test_can_list_albums(self):
         '''
-        View all albums
+        All users can view all albums
         '''
         admin = User.objects.get(username='admin')
-        Album.objects.create(owner=admin, title='a title')
+        Album.objects.create(owner=admin, title='a title', cover_image='cover')
         response = self.client.get('/albums/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_logged_in_user_can_create_album(self):
         '''
-        Create Album with logged in user
+        Logged in user can create albums with default image
         '''
         self.client.login(username='admin', password='pass')
         response = self.client.post('/albums/', {'title': 'a title'})
         count = Album.objects.count()
         self.assertEqual(count, 1)
+        album = Album.objects.filter(pk=1).first()
+        self.assertEqual(album.cover_image, '../default_post_liudmg')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_logged_in_user_can_create_album_with_upload_image(self):
+        '''
+        Logged in user can create albums with upload image
+        '''
+        self.client.login(username='admin', password='pass')
+        admin = User.objects.get(username='admin')
+        album = Album.objects.create(
+            owner=admin, title='a title', content='admin content',
+            cover_image='UploadImage')
+        count = Album.objects.count()
+        self.assertEqual(count, 1)
+        album = Album.objects.filter(pk=1).first()
+        self.assertEqual(album.cover_image, 'UploadImage')
 
     def test_logged_out_user_cant_create_album(self):
         '''
-        Create Album with logged out user
+        Not allow to create album while logged out
         '''
         response = self.client.post('/albums/', {'title': 'a title'})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
@@ -54,10 +73,19 @@ class AlbumDetailViewTests(APITestCase):
         '''
         admin = User.objects.create_user(username='admin', password='pass')
         user1 = User.objects.create_user(username='user1', password='pass')
-        Album.objects.create(
+        album1 = Album.objects.create(
             owner=admin, title='a title', content='admin content')
-        Album.objects.create(
+        album2 = Album.objects.create(
             owner=user1, title='a title', content='user1 content')
+        # Album's children items
+        Photo.objects.create(owner=admin, album=album1, title='photo 1')
+        Photo.objects.create(owner=admin, album=album1, title='photo 2')
+        Photo.objects.create(owner=admin, album=album2, title='photo 1')
+        Comment.objects.create(owner=admin, album=album1, content='comment 1')
+        Comment.objects.create(owner=admin, album=album1, content='comment 2')
+        Comment.objects.create(owner=admin, album=album2, content='comment 1')
+        Like.objects.create(owner=admin, album=album1)
+        Like.objects.create(owner=user1, album=album1)
 
     def test_can_retrieve_album_using_valid_id(self):
         '''
@@ -86,7 +114,7 @@ class AlbumDetailViewTests(APITestCase):
 
     def test_user_cant_update_another_user_album(self):
         '''
-        Edit another user's album
+        Not allow to edit another user's album
         '''
         self.client.login(username='admin', password='pass')
         response = self.client.put('/albums/2', {'title': 'a new title'})
@@ -94,7 +122,50 @@ class AlbumDetailViewTests(APITestCase):
 
     def test_logged_out_user_cant_update_album(self):
         '''
-        Edit album without login
+        Not allow to edit album without logged in
         '''
         response = self.client.put('/albums/1', {'title': 'a new title'})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_user_can_delete_own_album(self):
+        '''
+        Delete own album
+        '''
+        self.client.login(username='admin', password='pass')
+        response = self.client.delete('/albums/1')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_user_cant_delete_another_user_album(self):
+        '''
+        Not allow to delete other user's album
+        '''
+        self.client.login(username='admin', password='pass')
+        response = self.client.delete('/albums/2')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_logged_out_user_cant_delete_album(self):
+        '''
+        Not allow to delete album without logged in
+        '''
+        response = self.client.delete('/albums/1')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_user_can_delete_own_album_and_contents(self):
+        '''
+        Delete own album and all children
+        '''
+        self.client.login(username='admin', password='pass')
+        count = Photo.objects.count()
+        self.assertEqual(count, 3)
+        count = Comment.objects.count()
+        self.assertEqual(count, 3)
+        count = Like.objects.count()
+        self.assertEqual(count, 2)
+        response = self.client.delete('/albums/1')
+        count = Photo.objects.count()
+        self.assertEqual(count, 1)
+        count = Comment.objects.count()
+        self.assertEqual(count, 1)
+        count = Like.objects.count()
+        self.assertEqual(count, 0)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
